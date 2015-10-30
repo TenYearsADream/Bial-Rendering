0cf2a0ffb7f324f3e37509cf87088e5d17bb5995
Lucas Santana Lellis <lucaslellis777@gmail.com>
2015-10-30 12:29:11 -0200

    Fixed normals

--- OpenGLView/stlviewer.cpp
+++ OpenGLView/stlviewer.cpp
@@ -2,6 +2,7 @@
 #include <QDebug>
 #include <QKeyEvent>
 #include <QOpenGLFunctions>
+#include <QTime>
 
 #include "MarchingCubes.hpp"
 #include "glassert.h"
@@ -24,10 +25,13 @@ void STLViewer::LoadFile( QString fileName ) {
     mesh = Bial::TriangleMesh::ReadSTLB( fileName.trimmed( ).toStdString( ) );
   }
   else {
+    QTime t;
+    t.start( );
+    qDebug( ) << "Running marching cubes algorithm.";
     Bial::Image< int > img = Bial::Geometrics::Scale( Bial::File::Read< int >( fileName.toStdString( ) ), 0.25, true );
 
     mesh = Bial::MarchingCubes::exec( img, 50.f );
-
+    qDebug( ) << "Elapsed: " << t.elapsed( ) << " ms";
   }
   /*    mesh->Print( std::cout ); */
   size_t *vertexIndex = mesh->getVertexIndex( );
@@ -51,12 +55,12 @@ void STLViewer::LoadFile( QString fileName ) {
   }
   if( n != nullptr ) {
     COMMENT( "Reading normals.", 0 );
-    norms = new GLdouble[ mesh->getNtris( ) * 3 ];
-    for( size_t t = 0; t < mesh->getNtris( ); ++t ) {
+    norms = new GLdouble[ mesh->getNverts( ) * 3 ];
+    for( size_t t = 0; t < mesh->getNverts( ); ++t ) {
       const Bial::Normal &norm = n[ t ];
-      norms[ t * 3 ] = static_cast< GLdouble >( norm.x );
-      norms[ t * 3 + 1 ] = static_cast< GLdouble >( norm.y );
-      norms[ t * 3 + 2 ] = static_cast< GLdouble >( norm.z );
+      norms[ t * 3 ] = 1.0 - static_cast< GLdouble >( norm.x );
+      norms[ t * 3 + 1 ] = 1.0 - static_cast< GLdouble >( norm.y );
+      norms[ t * 3 + 2 ] = 1.0 - static_cast< GLdouble >( norm.z );
       /*      std::cout << norm << std::endl; */
     }
   }
@@ -68,7 +72,7 @@ void STLViewer::LoadFile( QString fileName ) {
   boundings[ 2 ] = zs;
 }
 
-GLfloat lightPos[] = { 0.0f, 0.0f, 500.0f, 10 };
+GLfloat lightPos[] = { 0.0f, 0.0f, 5.0f, 10 };
 GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 GLfloat specref[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 GLfloat ambientLight[] = { 0.5f, 0.5f, 0.5f, 0.5f };
@@ -91,7 +95,7 @@ void STLViewer::initializeGL( ) {
   glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
   /* Enable OpenGL Antialiasing functions. */
   glEnable( GL_LINE_SMOOTH );
-  glEnable( GL_POLYGON_SMOOTH );
+/*  glEnable( GL_POLYGON_SMOOTH ); */
 
   glCheckError( );
 }
@@ -105,12 +109,16 @@ void STLViewer::resizeGL( int w, int h ) {
   glLoadIdentity( );
 /*  gluLookAt( 0, 0, -2, 0, 0, 0, 0, 1, 0 ); */
   if( verts ) {
-    qDebug() << "Verts found";
+    qDebug( ) << "Verts found";
+    glEnableClientState( GL_VERTEX_ARRAY );
     glVertexPointer( 3, GL_DOUBLE, 0, verts );
+    glDisableClientState( GL_VERTEX_ARRAY );
   }
   if( norms ) {
-    qDebug() << "Norms found";
+    qDebug( ) << "Norms found";
+    glEnableClientState( GL_NORMAL_ARRAY );
     glNormalPointer( GL_DOUBLE, 0, norms );
+    glDisableClientState( GL_NORMAL_ARRAY );
   }
   glCheckError( );
 }
@@ -133,25 +141,29 @@ void STLViewer::paintGL( ) {
   glScaled( 1.0 / boundings[ 0 ], 1.0 / boundings[ 1 ], 1.0 / boundings[ 2 ] );
 
 
-//  GLfloat red[] = { 0.2f, 0.2f, 0.2f, 1.f };
-//  glMaterialfv( GL_FRONT, GL_DIFFUSE, red );
-//
-//  GLfloat blue[] = { 0.f, 0.f, 0.8f, 1.f };
-//  glMaterialfv( GL_BACK, GL_DIFFUSE, blue );
+/*
+ *  GLfloat red[] = { 0.2f, 0.2f, 0.2f, 1.f };
+ *  glMaterialfv( GL_FRONT, GL_DIFFUSE, red );
+ *
+ *  GLfloat blue[] = { 0.f, 0.f, 0.8f, 1.f };
+ *  glMaterialfv( GL_BACK, GL_DIFFUSE, blue );
+ */
 
 
-  glColor4f( 1, 1, 1, 0.5 );
+  glColor4f( 1, 1, 1, 1 );
 
   glTranslated( -boundings[ 0 ] / 2.0, -boundings[ 1 ] / 2.0, -boundings[ 2 ] / 2.0 );
+  glEnable( GL_NORMALIZE );
+  glEnableClientState( GL_NORMAL_ARRAY );
   glEnableClientState( GL_VERTEX_ARRAY );
   glEnable( GL_POLYGON_OFFSET_FILL );
   glPolygonOffset( 1, 1 );
   if( tris ) {
-    glDrawElements( GL_TRIANGLES, mesh->getNtris( ) * 3, GL_UNSIGNED_INT, tris );
+    glAssert( glDrawElements( GL_TRIANGLES, mesh->getNtris( ) * 3, GL_UNSIGNED_INT, tris ) );
   }
   glDisable( GL_POLYGON_OFFSET_FILL );
   glDisableClientState( GL_VERTEX_ARRAY );
-
+  glDisableClientState( GL_NORMAL_ARRAY );
   glPopMatrix( );
 
   glBegin( GL_LINES );
